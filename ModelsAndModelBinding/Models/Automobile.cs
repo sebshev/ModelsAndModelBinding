337004b8c30c527ecd99cfa45e84dfb30dd6da90
Sebastian Shevlin <seb112697@gmail.com>
2021-01-25 01:44:31 -0800

    Changed from Student to Automobile

---
+++ ModelsAndModelBinding/Models/Automobile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelsAndModelBinding.Models
+{
+    public class Automobile
+    {
+        /// <summary>
+        /// The unique 9 digit ID number for the automobile
+        /// </summary>
+        public int VinNum { get; set; }
+
+        /// <summary>
+        /// The maker of the car Ex. Honda
+        /// </summary>
+        public string Manufacturer { get; set; }
+
+        /// <summary>
+        /// Title of the car Ex. Civic
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The year the car was manufactured
+        /// </summary>
+        public int Year { get; set; }
+    }
+}
