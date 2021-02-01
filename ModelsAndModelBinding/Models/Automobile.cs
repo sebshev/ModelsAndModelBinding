9b295f22b215da84a38de67086a3977b44c46e35
Sebastian Shevlin <seb112697@gmail.com>
2021-01-31 16:55:55 -0800

    Made the view using model binding :)

--- ModelsAndModelBinding/Models/Automobile.cs
+++ ModelsAndModelBinding/Models/Automobile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,21 +11,27 @@ namespace ModelsAndModelBinding.Models
         /// <summary>
         /// The unique 9 digit ID number for the automobile
         /// </summary>
+        [Display(Name = "VIN Number")]
+        [Required]
         public string VinNum { get; set; }
 
         /// <summary>
         /// The maker of the car Ex. Honda
         /// </summary>
-        public string Manufacturer { get; set; }
+        [Display(Name = "Make")]
+        [Required]
+        public string Make { get; set; }
 
         /// <summary>
         /// Title of the car Ex. Civic
         /// </summary>
+        [Required]
         public string Title { get; set; }
 
         /// <summary>
         /// The year the car was manufactured
         /// </summary>
+        [Required]
         public string Year { get; set; }
     }
 }
